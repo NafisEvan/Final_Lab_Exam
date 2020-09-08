23aaae90f242ad8373747414d22e44c70e36f636
Your Name <you@example.com>
2020-09-08 13:35:59 +0600

    registraction

---
+++ resources/views/Registraction/Registraction.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+<form method="post">
+    <table>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <tr>
+                <td>EmployerName</td>
+                <td><input type="text" name="EmployerNamename"></td>
+            </tr>
+            <tr>
+                <td>Company Name</td>
+                <td><input type="text" name="Companyname"></td>
+            </tr>
+            <tr>
+                <td>Phone</td>
+                <td><input type="text" name="Phonename"></td>
+            </tr>
+            <tr>
+                <td>Type</td>
+                <td><input type="text" name="Typename"></td>
+            </tr>
+            <td></td>
+            <td><input type="submit" name="submit" value="Submit"></td>
+        </tr>
+    </table>
+</body>
+</form>
+</html>
